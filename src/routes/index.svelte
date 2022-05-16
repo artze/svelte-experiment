8f7d7a0f1aac777447b466d03601d6eb405a4921
artze <arthurwtm@gmail.com>
2022-05-16 13:48:37 +0800

    Add about page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+	let title = 'foobar';
+</script>
+
+<h1>Welcome to {title}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
